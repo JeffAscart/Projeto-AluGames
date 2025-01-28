c938281f1103bb159f8167670c81718f0b92482d
Jefferson Duraes <jefferson.vduraes@gmail.com>
2025-01-27 21:01:49 -0300

    base Code

---
+++ js/app.js
@@ -0,0 +1,35 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')){
+        imagem.classList.remove('dashboard__item__img--rented');
+//        alert ('teste 4')
+    }
+    if (botao.classList.contains('dashboard__item__button--return')){
+        botao.classList.remove('dashboard__item__button--return');
+        botao.innerHTML = 'Alugar';
+//        alert ('Teste 2')
+        return
+    } 
+
+    if (imagem.classList.contains('dashboard__item__img')){
+        imagem.classList.add('dashboard__item__img--rented');
+//        alert ('teste 3')
+    }
+    if (botao.classList.contains('dashboard__item__button')){
+        botao.classList.add('dashboard__item__button--return');
+        botao.innerHTML = 'Devolver';
+//        alert ('teste 1')
+        return
+    }
+
+
+        
+
+
+    
+   // let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+   // alert(nomeJogo.textContent);
+}
